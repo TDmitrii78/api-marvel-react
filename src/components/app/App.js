918ab757d123a99e5f43f9d90139fd51b7850d1a
dmitrii <hdmail2022@gmail.com>
2023-04-11 01:18:07 +0400

    my commit)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,6 +5,7 @@ import ErrorBoundaries from "../errorBoundaries/ErrorBoundaries";
 import MainPage from '../page/MainPage';
 import ComicsPage from "../page/ComicsPage";
 import SingleComicPage from "../page/SingleComicPage";
+import Page404 from "../page/404";
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
                       <Route path="/" element={<MainPage/>}/>
                       <Route path="/comics" element={<ComicsPage/>}/>
                       <Route path='/comics/:comicsId' element={<SingleComicPage/>}/>
+                      <Route path="*"  element={<Page404/>}/>
                     </Routes> 
                 </main>
             </div>
